f8a6578a2fb4802e1a4d27cb04d248b3eccd65b2
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2026-02-09 18:45:07 +0530

    added appointment model to connect doctor and patient

---
+++ backend/models/appointment.model.js
@@ -0,0 +1,77 @@
+const { default: mongoose } = require('mongoose');
+
+const triageSchema = new mongoose.Schema({
+  symptoms: [String],
+  vitals: {
+    heartRate: Number,
+    bloodPressure: String,
+    temperature: Number,
+  },
+  comorbidities: [String],
+  priorityScore: Number,
+  severityLevel: {
+    type: String,
+    enum: ['low', 'medium', 'high', 'emergency'],
+  },
+  recomendedSpecilization: String,
+  createdAt: Date.now(),
+  source: String,
+});
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Patient',
+      required: true,
+    },
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Doctor',
+      required: true,
+    },
+
+    scheduledDate: {
+      type: Date,
+      required: true,
+    },
+    scheduledTime: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: [
+        'confirmed',
+        'checked_in',
+        'in_consulation',
+        'completed',
+        'cancelled',
+        'no_show',
+      ],
+      default: 'confirmed',
+    },
+    triage: {
+      type: triageSchema,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      enum: ['patient', 'staff'],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+appointmentSchema.index({
+  doctorId: 1,
+  scheduledDate: 1,
+  scheduledTime: 1,
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;

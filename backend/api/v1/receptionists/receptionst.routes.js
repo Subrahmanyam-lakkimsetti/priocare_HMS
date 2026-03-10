26305f8fffa614488772eac05de2932cbab58307
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2026-03-10 19:44:59 +0530

    updated receptionist routes

--- backend/api/v1/receptionists/receptionst.routes.js
+++ backend/api/v1/receptionists/receptionst.routes.js
@@ -6,14 +6,31 @@ const {
 const {
   createReceptionistController,
 } = require('./receptionistAuth.controller');
-const { patientCheckin } = require('./receptionist.controller');
+const {
+  patientCheckin,
+  dashboardStats,
+  todaysAppointments,
+  appointmentBytoken,
+  queueStats,
+  recentCheckins,
+} = require('./receptionist.controller');
 
 const receptionistRouter = express.Router();
 
-receptionistRouter.use(authMiddleware);
-(receptionistRouter.use(restrictTo('receptionist')),
-  receptionistRouter.post('/', createReceptionistController));
+receptionistRouter.use(authMiddleware, restrictTo('receptionist'));
+
+receptionistRouter.post('/', createReceptionistController);
 
 receptionistRouter.patch('/patient-checkin/token/:token', patientCheckin);
+
+receptionistRouter.get('/dashboard', dashboardStats);
+
+receptionistRouter.get('/appointments', todaysAppointments);
+
+receptionistRouter.get('/appointments/token/:token', appointmentBytoken);
+
+receptionistRouter.get('/queues', queueStats);
+
+receptionistRouter.get("/checkins/recent", recentCheckins)
 
 module.exports = receptionistRouter;
